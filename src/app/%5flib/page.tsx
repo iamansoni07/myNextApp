0cac0767f39f615415771faa343258078a77ebd6
iamansoni07 <aman.soni0713@gmail.com>
2025-08-27 15:51:27 +0530

    Add initial pages and components for authentication and dashboard

---
+++ src/app/%5flib/page.tsx
@@ -0,0 +1,8 @@
+export default function privateRoute(){
+    return (
+        <div>
+            <h1>Private Route</h1>
+            <p>This is a private route.</p>
+        </div>
+    );
+}
---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,8 @@
+export default function lineChart() {
+    return (
+        <div>
+            <h1>Line Chart Page</h1>
+            <p>This is the line chart page.</p>
+        </div>
+    );
+}
